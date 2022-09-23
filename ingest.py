bd8b7afce3e016af78bc808625054021a1b4d2b0
Jacob Tews <jacob.tews@cnu.edu>
2022-09-22 23:09:00 -0400

    beginning process of ingestion

---
+++ ingest.py
@@ -0,0 +1,13 @@
+"""
+The purpose of this script is to retrieve the needed information from the Spotify API.
+
+"""
+
+import spotipy
+
+# tasks:
+# retrieve Spotify data for 20 artists (must be at least 1000 songs)
+
+
+if __name__ == '__main__':
+    pass
